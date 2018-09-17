fd782b92573ae3a0ef57e074147984809aadae82
zdeptawa <zachary.deptawa@microsoft.com>
2018-09-17 15:51:57 -0500

    initial commit

---
+++ demos/azurerm_tf_aks_ignite_demo/k8s_pod.tf
@@ -0,0 +1,46 @@
+provider "kubernetes" {
+  host = "${azurerm_kubernetes_cluster.k8s.kube_config.0.host}"
+
+  #username               = "${azurerm_kubernetes_cluster.k8s.kube_config.0.username}"
+  #password               = "${azurerm_kubernetes_cluster.k8s.kube_config.0.password}"
+  client_certificate = "${base64decode(azurerm_kubernetes_cluster.k8s.kube_config.0.client_certificate)}"
+
+  client_key             = "${base64decode(azurerm_kubernetes_cluster.k8s.kube_config.0.client_key)}"
+  cluster_ca_certificate = "${base64decode(azurerm_kubernetes_cluster.k8s.kube_config.0.cluster_ca_certificate)}"
+}
+
+resource "kubernetes_pod" "ignite-pod" {
+  metadata {
+    name = "terraform-example"
+
+    #name = "ignite-k8s-nginx-pod"
+  }
+
+  spec {
+    container {
+      image = "nginx:1.7.9"
+      name  = "example"
+    }
+  }
+}
+
+resource "kubernetes_service" "web" {
+  metadata {
+    name = "ignite-k8s-nginx-service"
+  }
+
+  spec {
+    selector {
+      name = "${kubernetes_pod.ignite-pod.metadata.0.name}"
+    }
+
+    session_affinity = "ClientIP"
+
+    port {
+      port        = 80
+      target_port = 80
+    }
+
+    type = "LoadBalancer"
+  }
+}
---
+++ demos/azurerm_tf_aks_ignite_demo/variables.tf
@@ -0,0 +1,31 @@
+variable "client_id" {
+  default = ""
+}
+
+variable "client_secret" {
+  default = ""
+}
+
+variable "agent_count" {
+  default = 3
+}
+
+variable "ssh_public_key" {
+  default = "~/.ssh/id_rsa.pub"
+}
+
+variable "dns_prefix" {
+  default = "k8stest"
+}
+
+variable "cluster_name" {
+  default = "k8stest"
+}
+
+variable "resource_group_name" {
+  default = "ignite-azure-k8stest"
+}
+
+variable location {
+  default = "East US 2"
+}
