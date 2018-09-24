7784de6dcfa6570c6b28ccd13853acc50d732e5d
zdeptawa <zachary.deptawa@microsoft.com>
2018-09-24 00:20:35 -0400

    add config output

--- demos/azurerm_tf_aks_ignite_demo/output.tf
+++ demos/azurerm_tf_aks_ignite_demo/output.tf
@@ -25,3 +25,13 @@ output "kube_config" {
 output "host" {
   value = "${azurerm_kubernetes_cluster.k8s.kube_config.0.host}"
 }
+
+output "configure" {
+  value = <<CONFIGURE
+Run the following commands to configure kubernetes client:
+$ terraform output kube_config > ~/.kube/aksconfig
+$ export KUBECONFIG=~/.kube/aksconfig
+Test configuration using kubectl
+$ kubectl get nodes
+CONFIGURE
+}
