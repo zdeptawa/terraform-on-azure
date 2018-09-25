a264e089fe10e46fa5fa6bf63a34f3974d7cc21f
zdeptawa <zachary.deptawa@microsoft.com>
2018-09-24 20:39:11 -0400

    creating ssh key, updating auth

--- demos/azurerm_tf_aks_ignite_demo/k8s.tf
+++ demos/azurerm_tf_aks_ignite_demo/k8s.tf
@@ -1,3 +1,24 @@
+# Create private key for the kubernetes cluster
+resource "tls_private_key" "key" {
+  algorithm   = "RSA"
+}
+
+# Save the private key in the local workspace
+resource "null_resource" "save-key" {
+  triggers {
+    key = "${tls_private_key.key.private_key_pem}"
+  }
+
+  # Use 'local-exec' to put the key in local .ssh folder and set perms
+  provisioner "local-exec" {
+    command = <<EOF
+      mkdir -p ${path.module}/.ssh
+      echo "${tls_private_key.key.private_key_pem}" > ${path.module}/.ssh/id_rsa
+      chmod 0600 ${path.module}/.ssh/id_rsa
+EOF
+  }
+}
+
 # Create the resource group for our k8s environment
 resource "azurerm_resource_group" "k8s" {
   name     = "${var.resource_group_name}"
@@ -17,10 +38,10 @@ resource "azurerm_kubernetes_cluster" "k8s" {
   dns_prefix          = "${var.dns_prefix}"
 
   linux_profile {
-    admin_username = "ubuntu"
+    admin_username = "${var.admin_username}"
 
     ssh_key {
-      key_data = "${file("${var.ssh_public_key}")}"
+      key_data = "${trimspace(tls_private_key.key.public_key_openssh)} ${var.admin_username}@azure.com"
     }
   }
 
