990a4d0025a9cd9a5033844d5dd765133ebc0c5a
Zachary Deptawa <zdeptawa@pthwin1.redmond.corp.microsoft.com>
2020-07-06 13:03:06 -0500

    initial commit of new demo

---
+++ demos/azurerm_tf_dual_aks_cluster_example/variables.tf
@@ -0,0 +1,55 @@
+variable "client_id" {
+  default = ""
+}
+
+variable "client_secret" {
+  default = ""
+}
+
+variable "environment_tag" {
+  default = "development"
+}
+
+variable "build_tag" {
+  default = "k8sdemo"
+}
+
+variable "agent_count" {
+  default = 3
+}
+
+variable "admin_username" {
+  default = "ubuntu"
+}
+
+variable "dns_prefix_east" {
+  default = "k8sdemoeast"
+}
+
+variable "dns_prefix_west" {
+  default = "k8sdemowest"
+}
+
+variable "cluster_name_east" {
+  default = "k8s-demo-cluster-east"
+}
+
+variable "cluster_name_west" {
+  default = "k8s-demo-cluster-west"
+}
+
+variable "resource_group_name_east" {
+  default = "k8s-demo-resource-group-east"
+}
+
+variable "resource_group_name_west" {
+  default = "k8s-demo-resource-group-west"
+}
+
+variable "location_east" {
+  default = "East US"
+}
+
+variable "location_west" {
+  default = "West US"
+}
