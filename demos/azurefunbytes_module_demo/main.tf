49eb68a2c040c5ddda68f621bea76b265d0f171c
Zachary Deptawa <zdeptawa@zacharys-mbp-2.lan>
2021-01-13 19:03:38 -0600

    updated to use modules

--- demos/azurefunbytes_module_demo/main.tf
+++ demos/azurefunbytes_module_demo/main.tf
@@ -10,117 +10,44 @@ provider "azurerm" {
 
 # Create a resource group
 resource "azurerm_resource_group" "azfunbytes_resource_group" {
-  name     = "azfunbytes_demo"
+  name     = "azfunbytes_demo_2"
   location = "West US 2"
 
-  tags     = { environment = "demo", build = "azfunbytes" }
+  tags     = { environment = "demo", build = "azfunbytes2" }
 }
 
 # Create a network
-resource "azurerm_virtual_network" "azfunbytes_network" {
-  name                = "azfunbytes-network"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.azfunbytes_resource_group.location
+module "azfunbytes_network" {
+  source              = "Azure/network/azurerm"
   resource_group_name = azurerm_resource_group.azfunbytes_resource_group.name
-  tags     = { environment = "demo", build = "azfunbytes" }
+  subnet_prefixes     = ["10.0.2.0/24"]
+  subnet_names        = ["azfunbytes_internal_subnet"]
+  tags                = { environment = "demo", build = "azfunbytes2" }
+
+  depends_on = [azurerm_resource_group.azfunbytes_resource_group]
 }
 
-# Create a subnet
-resource "azurerm_subnet" "azfunbytes_internal_subnet" {
-  name                 = "azfunbytes_internal"
-  resource_group_name  = azurerm_resource_group.azfunbytes_resource_group.name
-  virtual_network_name = azurerm_virtual_network.azfunbytes_network.name
-  address_prefixes     = [ "10.0.2.0/24" ]
-}
+# Create a virtual machine
+module "azfunbytes_linux_vm" {
+  source                = "Azure/compute/azurerm"
+  resource_group_name   = azurerm_resource_group.azfunbytes_resource_group.name
+  vm_hostname           = "azfunbytes-web"
+  vm_size               = "Standard_F2"
+  vm_os_simple          = "UbuntuServer"
+  admin_username        = "adminuser"
+  ssh_key               = "~/.ssh/id_rsa.pub"
+  storage_account_type  = "Standard_LRS"
+  remote_port           = "80"
+  vnet_subnet_id        = module.azfunbytes_network.vnet_subnets[0]
+  tags                  = { environment = "demo", build = "azfunbytes2" }
 
-# Create a security group to allow port 80 access
-resource "azurerm_network_security_group" "azfunbytes_sg" {
-    name                = "azfunbytes_security_group"
-    location            = azurerm_resource_group.azfunbytes_resource_group.location
-    resource_group_name = azurerm_resource_group.azfunbytes_resource_group.name
-    
-    security_rule {
-        name                       = "HTTP"
-        priority                   = 1001
-        direction                  = "Inbound"
-        access                     = "Allow"
-        protocol                   = "Tcp"
-        source_port_range          = "*"
-        destination_port_range     = "80"
-        source_address_prefix      = "*"
-        destination_address_prefix = "*"
-    }
-
-    tags = { environment = "demo", build = "azfunbytes" }
-}
-
-# Create a public IP for our virtual machine
-resource "azurerm_public_ip" "azfunbytes_public_ip" {
-    name                         = "azfunbytes_public_ip"
-    location                     = azurerm_resource_group.azfunbytes_resource_group.location
-    resource_group_name          = azurerm_resource_group.azfunbytes_resource_group.name
-    allocation_method            = "Dynamic"
-
-    tags = { environment = "demo", build = "azfunbytes" }
-}
-
-# Create a network interface for our virtual machine with both a private and public IP
-resource "azurerm_network_interface" "azfunbytes_interface" {
-  name                = "azfunbytes-nic"
-  location            = azurerm_resource_group.azfunbytes_resource_group.location
-  resource_group_name = azurerm_resource_group.azfunbytes_resource_group.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.azfunbytes_internal_subnet.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.azfunbytes_public_ip.id
-  }
-
-  tags     = { environment = "demo", build = "azfunbytes" }
-}
-
-# Associate our network interface with the security group we created to allow port 80 access
-resource "azurerm_network_interface_security_group_association" "azfunbytes_sg_assoc" {
-    network_interface_id      = azurerm_network_interface.azfunbytes_interface.id
-    network_security_group_id = azurerm_network_security_group.azfunbytes_sg.id
-}
-
-# Create our linux VM, assigning the proper network interface
-resource "azurerm_linux_virtual_machine" "azfunbytes_linux_vm" {
-  name                = "azfunbytes-linux-machine"
-  resource_group_name = azurerm_resource_group.azfunbytes_resource_group.name
-  location            = azurerm_resource_group.azfunbytes_resource_group.location
-  size                = "Standard_F2"
-  admin_username      = "adminuser"
-  network_interface_ids = [
-    azurerm_network_interface.azfunbytes_interface.id,
-  ]
-
-  admin_ssh_key {
-    username   = "adminuser"
-    public_key = file("~/.ssh/id_rsa.pub")
-  }
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
-    version   = "latest"
-  }
-
-  tags     = { environment = "demo", build = "azfunbytes" }
+  depends_on = [azurerm_resource_group.azfunbytes_resource_group]
 }
 
 # Run a sample script to update the server and install nginx after it's done building
 resource "azurerm_virtual_machine_extension" "azfunbytes_vm_web_build" {
   name                 = "azfunbytes_vm_web_build"
-  virtual_machine_id   = azurerm_linux_virtual_machine.azfunbytes_linux_vm.id
+  virtual_machine_id   = module.azfunbytes_linux_vm.vm_ids[0]
   publisher            = "Microsoft.Azure.Extensions"
   type                 = "CustomScript"
   type_handler_version = "2.0"
@@ -132,4 +59,10 @@ resource "azurerm_virtual_machine_extension" "azfunbytes_vm_web_build" {
 SETTINGS
 
   tags = { environment = "demo", build = "azfunbytes" }
+
+  depends_on = [module.azfunbytes_linux_vm]
+}
+
+output "linux_vm_public_name" {
+  value = module.azfunbytes_linux_vm.public_ip_address
 }
